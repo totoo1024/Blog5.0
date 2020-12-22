410a12617a4da268ed539b14e76e825bccbd40eb
念舊 <823302316@qq.com>
2020-12-22 10:24:27 +0800

    no commit message

--- src/App.Framwork/DependencyInjection/Attributes/SectionAttribute.cs
+++ src/App.Framwork/DependencyInjection/Attributes/SectionAttribute.cs
@@ -12,31 +12,9 @@ namespace App.Framwork.DependencyInjection.Attributes
         /// appsettings节点
         /// </summary>
         public string Section { get; }
-
-        public Lifetime Lifetime { get; }
-        public SectionAttribute(string section, Lifetime lifetime = Lifetime.Scoped)
+        public SectionAttribute(string section)
         {
             Section = section;
-            Lifetime = Lifetime;
         }
-    }
-
-    /// <summary>
-    /// 生命周期
-    /// </summary>
-    public enum Lifetime
-    {
-        /// <summary>
-        /// 瞬时
-        /// </summary>
-        Transient,
-        /// <summary>
-        /// 范围
-        /// </summary>
-        Scoped,
-        /// <summary>
-        /// 单列
-        /// </summary>
-        Singleton
     }
 }
