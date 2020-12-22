3defadcbf3c72474fe5b28f78097c635af91a940
念舊 <823302316@qq.com>
2020-12-22 10:38:46 +0800

    删除无用引用命名空间

--- src/App.Framwork/DependencyInjection/Extensions/ServiceCollectionExtensions.cs
+++ src/App.Framwork/DependencyInjection/Extensions/ServiceCollectionExtensions.cs
@@ -1,11 +1,8 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
+﻿using System.Linq;
 using App.Framwork.DependencyInjection.Attributes;
 using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
 using Microsoft.Extensions.DependencyInjection.Extensions;
-using Microsoft.Extensions.Options;
 
 namespace App.Framwork.DependencyInjection.Extensions
 {
