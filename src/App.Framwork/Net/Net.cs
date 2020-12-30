3919be57addf5c9ea476a0083ccfbaf0802bb7f7
念舊 <823302316@qq.com>
2020-12-30 15:54:19 +0800

    删除无用名称空间

--- src/App.Framwork/Net/Net.cs
+++ src/App.Framwork/Net/Net.cs
@@ -3,7 +3,6 @@ using System.Collections.Generic;
 using System.Net.Http;
 using System.Text;
 using System.Threading.Tasks;
-using Microsoft.AspNetCore.Http;
 using Microsoft.Net.Http.Headers;
 using Newtonsoft.Json;
 
